c182eeaf3a721a3eb5b1ed344f9aceac23835a71
Ebrahim-78 <144029665+Ebrahim-78@users.noreply.github.com>
2024-02-10 01:00:34 +0300

    add css style

---
+++ src/Navbar.js
@@ -0,0 +1,14 @@
+const Navbar = () => {
+    return (
+        <nav className="Navbar">
+            <h1>The do Jo blog</h1>
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/create">New Blog</a>
+            </div>
+
+        </nav>
+    );
+}
+ 
+export default Navbar;
