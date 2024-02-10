70620eb97a223a19ca0ec0b950e3c35fc9b90742
Ebrahim-78 <144029665+Ebrahim-78@users.noreply.github.com>
2024-02-10 23:33:46 +0300

    finished lesson  11

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,19 @@
+import { useState } from "react";
+import BlogList from "./Blog-list";
+
 const Home = () => {
+    // let name = 'mario'
+    const [blog, setBlogs] = useState([
+        {title: 'My new website', body:' lorem ispum',auther:'mario', id: 1},
+        {title: 'Welcom party', body:' lorem ispum',auther:'yoshi', id: 2},
+        {title: 'web dev top tips', body:' lorem ispum',auther:'Ebrahim', id:3},
+    ])
+
     return (
         <div className="home">
-            <h2>Home page</h2>
+        <BlogList blog={blog} title = "All Blogs" />
         </div>
     );
 }
- 
+
 export default Home
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,6 +1,6 @@
 const Navbar = () => {
     return (
-        <nav className="Navbar">
+        <nav className="navbar">
             <h1>The do Jo blog</h1>
             <div className="links">
                 <a href="/">Home</a>
