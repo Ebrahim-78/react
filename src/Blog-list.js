84af6424e104f5e35567989d8b3ef8f5812f11ff
Ebrahim-78 <144029665+Ebrahim-78@users.noreply.github.com>
2024-02-11 13:02:17 +0300

    add delet button

--- src/Blog-list.js
+++ src/Blog-list.js
@@ -1,12 +1,13 @@
-const BlogList = ({blog,title}) => {
+const BlogList = ({blog, title, handelDelete}) => { 
 
-    return ( 
+    return (  
         <div className="blog-list">
             <h2>{title}</h2>
             {blog.map((blog) => (
                 <div className="blog-preview" key={blog.id}>
                     <h2>{blog.title}</h2>
                     <p>Written by {blog.auther} </p>
+                    <button onClick={() => handelDelete(blog.id)}>Delete blog</button>
                 </div>
             ))}
         </div>
--- src/Home.js
+++ src/Home.js
@@ -1,17 +1,28 @@
-import { useState } from "react";
+import { useState , useEffect} from "react";
 import BlogList from "./Blog-list";
 
 const Home = () => {
-    // let name = 'mario'
     const [blog, setBlogs] = useState([
-        {title: 'My new website', body:' lorem ispum',auther:'mario', id: 1},
-        {title: 'Welcom party', body:' lorem ispum',auther:'yoshi', id: 2},
+        {title: 'My new website', body:' lorem ispum',auther:'mohammed', id: 1},
+        {title: 'Welcom party', body:' lorem ispum',auther:'Ebrahim', id: 2},
         {title: 'web dev top tips', body:' lorem ispum',auther:'Ebrahim', id:3},
+        {title: 'web dev top tips', body:' lorem ispum',auther:'Ebrahim', id:4},
+        {title: 'web dev top tips', body:' lorem ispum',auther:'mohammed', id:5},
     ])
+    const handelDelete =(id) => {
+        const newBlogs = blog.filter(blog => blog.id !== id )
+        setBlogs(newBlogs);
 
+    }
+    useEffect(() => {
+        console.log('use effect ran ')
+        console.log(blog)
+    });
     return (
         <div className="home">
-        <BlogList blog={blog} title = "All Blogs" />
+        <BlogList blog={blog} title = "All Blogs !!" handelDelete={handelDelete}/>
+        <BlogList blog={blog.filter((blog => blog.auther === 'Ebrahim'))} title = "Ebrahim's Blogs !!" handelDelete={handelDelete} />
+        <BlogList blog={blog.filter((blog => blog.auther === 'mohammed'))} title = "mohammed's Blogs !!" handelDelete={handelDelete} /> 
         </div>
     );
 }
