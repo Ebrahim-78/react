b5b733db2e7db018795e9a31843891c15f9c6387
Ebrahim-78 <144029665+Ebrahim-78@users.noreply.github.com>
2024-02-13 22:17:11 +0300

    add "notFound page"

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home  from './Home';
 import Create from './create';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import BlogDetails from './BlogDetails';
+import NotFound from './NotFound';
 function App() {
 
   // const person = {name :'ahmed' ,age: 15}
@@ -20,6 +21,9 @@ function App() {
             </Route>
             <Route  path="/blogs/:id">
               <BlogDetails/>
+            </Route>
+            <Route path= "*">
+              <NotFound/>
             </Route>
           </Switch>
         </div>
--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,8 +1,17 @@
-import { useParams } from "react-router-dom/cjs/react-router-dom.min";
+import { useHistory, useParams } from "react-router-dom/cjs/react-router-dom.min";
 import useFetch from "./useFectch";
 const BlogDetails = () => {
     const { id  } = useParams()
     const {data: blog , isLoading , error} = useFetch('http://localhost:8000/blogs/' + id)
+    const history = useHistory();
+    const handeClick = () => {
+        fetch('http://localhost:8000/blogs/' + blog.id, {
+            method: "DELETE",
+        })
+        .then(() => {
+            history.push('/');
+        })
+    }
     return ( 
         <div className="blog-details">
             {isLoading && <div>Loading ...</div>}
@@ -12,6 +21,7 @@ const BlogDetails = () => {
                     <h2> {BlogDetails.title} </h2>
                     <p>Written by {blog.author}</p>
                     <div>{blog.body}</div>
+                    <button onClick={handeClick}>Delet Blog</button>
                 </article>
             )}
         </div>
