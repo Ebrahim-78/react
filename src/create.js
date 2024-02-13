1d3fda2f9a9fe67bcc2f3665be0ae0e6eeec9f06
Ebrahim-78 <144029665+Ebrahim-78@users.noreply.github.com>
2024-02-13 22:05:10 +0300

    add a new blog and return to Home page

--- src/create.js
+++ src/create.js
@@ -1,13 +1,37 @@
 import { useState } from "react";
+import { useHistory } from "react-router-dom";
 
 const Create = () => {
     const [title ,settitle] = useState('');
     const [body ,setbody] = useState('');
     const [author ,setAuthor] = useState('yoshi');
+    const [isLoading,setIsLoading] = useState(false);
+    const history = useHistory();
+
+    const handelSubit = (e) => {
+        e.preventDefault();
+        const blog = {title ,body , author}
+        console.log(blog)
+        setIsLoading(true);
+
+        fetch('http://localhost:8000/blogs', {
+            method: 'POST',
+            headers: {"content-type":"application/json"},
+            body: JSON.stringify(blog)
+        })
+        .then(() => {
+            console.log("new blog add")
+            setIsLoading(false);
+            history.go('http://localhost:8000/blogs');
+            // history.push('/');
+
+
+        })
+    }
     return ( 
         <div className="create">
             <h2>Add a new Blog</h2>
-            <form>
+            <form onSubmit={handelSubit}>
                 <label>Blog title: </label>
                 <input
                 type="text"
@@ -29,10 +53,9 @@ const Create = () => {
                     <option value="mario">mario</option>
                     <option value="yoshi">yoshi</option>
                 </select>
-                <button>Add blog</button>
-                <p>{title}</p>
-                <p>{body}</p>
-                <p>{author}</p>
+                {!isLoading&&<button>Add blog</button>}
+                {isLoading&&<button disabled>adding blog ...</button>}
+                
             </form>
         </div>
     );
